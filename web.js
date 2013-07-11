2850848258a1199b7cd2cb46359a5aee4554fdc9
Ubuntu <ubuntu@ip-172-31-33-152.us-west-2.compute.internal>
2013-07-11 01:13:53 +0000

    Reading index in web.js3

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var response = fs.readFileSync("index.html","utf-8");
   response.send('Hello World 2!');
-  fs.readFileSync("index.html","utf-8");
 });
 
 var port = process.env.PORT || 5000;
